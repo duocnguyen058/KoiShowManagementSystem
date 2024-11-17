80a21285960cb886046b816152565cc6f72b529b
HauDTg <tronghau105@gmail.com>
2024-11-17 15:37:05 +0700

    hau

---
+++ KoiShowManagementSystem.Repositories/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace KoiShowManagementSystem.Repositories
+{
+    public class ApplicationDbContext : DbContext
+    {
+        
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+        {
+        }
+        public DbSet<Users> Users { get; set; }
+        public DbSet<Scores> Scores { get; set; }
+        public DbSet<Reports> Reports { get; set; }
+        public DbSet<Koi> Kois { get; set; }
+        public DbSet<JudgeAssignments> JudgeAssignments { get; set; }
+        public DbSet<Events> Events { get; set; }
+        public DbSet<Event_Koi_Participation> Event_Koi_Participations { get; set; }
+    }
+}
---
+++ KoiShowManagementSystem.Repositories/GlobalUsing.cs
@@ -0,0 +1,5 @@
+﻿global using KoiShowManagementSystem.Repositories.Data;
+global using KoiShowManagementSystem.Repositories.Entity;
+global using System.ComponentModel.DataAnnotations;
+global using System.ComponentModel.DataAnnotations.Schema;
+global using Microsoft.EntityFrameworkCore;
