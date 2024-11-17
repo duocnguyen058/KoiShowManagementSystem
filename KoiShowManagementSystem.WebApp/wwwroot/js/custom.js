e774199c40b04f94b60ce55a631bf67f930b0599
khanhlazy <khanhvps.mmo@gmail.com>
2024-11-17 16:05:42 +0700

    khánh

---
+++ KoiShowManagementSystem.WebApp/wwwroot/js/custom.js
@@ -0,0 +1,24 @@
+﻿// Wait for the DOM to be fully loaded
+document.addEventListener("DOMContentLoaded", function () {
+
+    // Navbar color change on scroll
+    const navbar = document.querySelector(".navbar");
+    window.addEventListener("scroll", function () {
+        if (window.scrollY > 50) {
+            navbar.classList.add("scrolled");
+        } else {
+            navbar.classList.remove("scrolled");
+        }
+    });
+
+    // Animate the buttons when hovering
+    const buttons = document.querySelectorAll(".btn-custom");
+    buttons.forEach(button => {
+        button.addEventListener("mouseover", () => {
+            button.classList.add("hovered");
+        });
+        button.addEventListener("mouseout", () => {
+            button.classList.remove("hovered");
+        });
+    });
+});
